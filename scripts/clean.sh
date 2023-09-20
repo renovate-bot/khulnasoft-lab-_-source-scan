c6d877a679509dbd75a23224ed50cfc4184c522d
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-20 12:28:51 +0600

    Create clean.sh

---
+++ scripts/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# shellcheck disable=SC2044
+
+# This is a build script to generate local containers to perform tests
+
+for FILENAME in $(find . -type f -name 'Dockerfile-*'); do
+  TAG=${FILENAME#*-}
+  docker rmi -f source-scan:"${TAG}"
+done
